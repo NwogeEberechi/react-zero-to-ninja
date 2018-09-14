2537a605507f38183d752b944e2addd89b6d2733
NwogeEberechi <eberechinwoge@gmail.com>
2018-09-14 12:14:35 +0100

    remove unnecessary comment

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 class Display extends Component {
   render () {
-    // const string = this.props.data.join('')
     return <div className="display"> 5 </div>
   }
 }
