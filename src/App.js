27b6ca9fcdd943aad5f32f9ca3f687632cd978c5
NwogeEberechi <eberechinwoge@gmail.com>
2018-09-14 09:03:27 +0100

    add display componet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Display from './Display'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Display />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Display.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './app.scss'
+
+class Display extends Component {
+  render () {
+    // const string = this.props.data.join('')
+    return <div className="Display"> 5 </div>
+  }
+}
+
+export default Display
